ce85cc33def220a78fd156f0d9e828d363d7a029
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2020-11-18 18:32:56 +0100

    hola

--- minishell.h
+++ minishell.h
@@ -14,6 +14,10 @@
 **	librerias de prueba (eliminar en la version final)
 */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+#endif
+
 #include "libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
