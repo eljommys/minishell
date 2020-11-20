7a687f557c826110c3e44cb4dd41d3dfb4e53d20
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2020-11-20 14:52:03 +0100

    hola

--- main.c
+++ main.c
@@ -27,8 +27,6 @@ static void	check_command(char *str)
 		else if (!ft_memcmp(str, "./", 2) || !ft_memcmp(str, "../", 3) ||
 				!ft_memcmp(str, "/", 1))
 			bash_command(str);
-		else if (!ft_memcmp(str, "cd ", 3))
-			cd_command(str);
 		else if (!ft_memcmp(str, "quit", 4) || !ft_memcmp(str, "exit", 4) ||
 				!ft_memcmp(str, "close", 5) || !ft_memcmp(str, "q", 1))
 			exit_command(str);
