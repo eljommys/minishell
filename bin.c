4436a174f3ce9b7004231646e88e5c8882b51b92
parismart <parismrt@gmail.com>
2020-11-28 19:49:02 +0100

    pull

--- bin.c
+++ bin.c
@@ -44,64 +44,13 @@ void		set_args(char **argv, char *str, int argc)
 	}
 }
 
-
-/*static int		is_coincidence(char *str, DIR **dir, struct dirent **d)
-{
-	int	is;
-
-	*dir = opendir("/bin");
-	is = 0;
-	while(*d = readdir(*dir))
-		if (!ft_memcmp(str, (*d)->d_name, ft_strlen((*d)->d_name)))
-		{
-			is = 1;
-			break ;
-		}
-	return (is);
-}
-
-int		check_bin(char *str, char **envp, int fd)
-{
-	DIR				*dir;
-	struct dirent	*d;
-	char			*path;
-	int				status_argc[4];
-	char			**argv;
-
-	if (status_argc[3] = is_coincidence(str, &dir, &d))
-	{
-		status_argc[2] = count_args(str);
-		argv = (char **)ft_calloc(sizeof(char *), (status_argc[2] + 1));
-		if (status_argc[2])
-			set_args(argv, str, status_argc[2]);
-		path = ft_strjoin("/bin/", d->d_name);
-		status_argc[0] = 0;
-		if (!fork())
-		{
-			if (fd > 1)
-				dup2(fd, 1);
-			if(execve(path, argv, envp))
-				write(1, "Coudn't execute command\n", 24);
-			status_argc[0] = 1;
-		}
-		else
-			wait(&status_argc[1]);
-		free(path);
-		if (status_argc[0])
-			exit(0);
-		free_env(argv);
-	}
-	closedir(dir);
-	return (status_argc[3]);
-}*/
-
 static char		*is_coincidence(char *str, DIR **dir, struct dirent **d, char **envp)
 {
 	char	*path_str;
 	char	**paths;
-	int		i;
 	char	*path;
 	char	*name;
+	int		i;
 
 	path_str = get_env(envp, "PATH");
 	paths = ft_split(path_str, ':');
@@ -136,11 +85,9 @@ int		check_bin(char *str, char **envp, int fd)
 	char			*pre_path;
 	int				status_argc[3];
 	char			**argv;
-	int				hola;
 
 	pre_path = is_coincidence(str, &dir, &d, envp);
 	status_argc[2] = 0;
-	hola = 0;
 	if (*pre_path)
 	{
 		status_argc[2] = 1;
@@ -153,7 +100,6 @@ int		check_bin(char *str, char **envp, int fd)
 		{
 			if (fd > 1)
 				dup2(fd, 1);
-			hola = 1;
 			if (execve(path, argv, envp))
 				write(1, "Coudn't execute command\n", 24);
 			exit(0);
@@ -163,75 +109,7 @@ int		check_bin(char *str, char **envp, int fd)
 		free(path);
 		free_env(argv);
 	}
-	if (hola)
-		exit(0);
 	closedir(dir);
 	free(pre_path);
 	return (status_argc[2]);
 }
-/*
-static char        *is_coincidence(char *str, DIR **dir, struct dirent **d, char **envp)
-{
-    char    *path_str;
-    char    **paths;
-    int        i;
-    char    *path;
-
-    path_str = get_env(envp, "PATH");
-    paths = ft_split(path_str, ':');
-    i = 0;
-    while (paths[i])
-    {
-        *dir = opendir(paths[i]);
-        while(*d = readdir(*dir))
-        {
-            if (!ft_memcmp(str, (*d)->d_name, ft_strlen_spa(str) + 1))
-            {
-                path = ft_strjoin(paths[i], "/");
-                free_env(paths);
-                return (path);
-            }
-        }
-        closedir(*dir);
-        i++;
-    }
-    free_env(paths);
-    return (0);
-}
-
-int        check_bin(char *str, char **envp, int fd)
-{
-    DIR                *dir;
-    struct dirent    *d;
-    char            *path;
-    char            *pre_path;
-    int                status_argc[3];
-    char            **argv;
-
-    if (pre_path = is_coincidence(str, &dir, &d, envp))
-    {
-        status_argc[1] = count_args(str);
-        argv = (char **)ft_calloc(sizeof(char *), (status_argc[1] + 1));
-        if (status_argc[1])
-            set_args(argv, str, status_argc[1]);
-        printf("d->name = %s\n", d->d_name);
-        printf("pre_path = %s\n", pre_path);
-        path = ft_strjoin(pre_path, d->d_name);
-        if (!fork())
-        {
-            if (fd > 1)
-                dup2(fd, 1);
-            if (execve(path, argv, envp))
-                write(1, "Coudn't execute command\n", 24);
-            exit(0);
-        }
-        else
-            wait(&status_argc[0]);
-        free(path);
-        free_env(argv);
-    }
-    closedir(dir);
-    free(pre_path);
-    return (status_argc[2]);
-}
-*/
