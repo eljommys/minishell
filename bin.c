feb9c4a30b9633c79b474305b89654f5d921d361
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2020-11-29 14:51:14 +0100

    pipes hechos

--- bin.c
+++ bin.c
@@ -44,57 +44,6 @@ void		set_args(char **argv, char *str, int argc)
 	}
 }
 
-
-/*static int		is_coincidence(char *str, DIR **dir, struct dirent **d)
-{
-	int	is;
-
-	*dir = opendir("/bin");
-	is = 0;
-	while(*d = readdir(*dir))
-		if (!ft_memcmp(str, (*d)->d_name, ft_strlen((*d)->d_name)))
-		{
-			is = 1;
-			break ;
-		}
-	return (is);
-}
-
-int		check_bin(char *str, char **envp, int fd)
-{
-	DIR				*dir;
-	struct dirent	*d;
-	char			*path;
-	int				status_argc[4];
-	char			**argv;
-
-	if (status_argc[3] = is_coincidence(str, &dir, &d))
-	{
-		status_argc[2] = count_args(str);
-		argv = (char **)ft_calloc(sizeof(char *), (status_argc[2] + 1));
-		if (status_argc[2])
-			set_args(argv, str, status_argc[2]);
-		path = ft_strjoin("/bin/", d->d_name);
-		status_argc[0] = 0;
-		if (!fork())
-		{
-			if (fd > 1)
-				dup2(fd, 1);
-			if(execve(path, argv, envp))
-				write(1, "Coudn't execute command\n", 24);
-			status_argc[0] = 1;
-		}
-		else
-			wait(&status_argc[1]);
-		free(path);
-		if (status_argc[0])
-			exit(0);
-		free_env(argv);
-	}
-	closedir(dir);
-	return (status_argc[3]);
-}*/
-
 static char		*is_coincidence(char *str, DIR **dir, struct dirent **d, char **envp)
 {
 	char	*path_str;
