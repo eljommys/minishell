b16ba72c234c2a242ba5eaed3d025eeef8cf93e2
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2020-12-07 18:33:14 +0100

    semicolon en comillas arreglado y las redirecciones (falta hacer que se interpreten como argumentos)

--- srcs/command.c
+++ srcs/command.c
@@ -6,13 +6,13 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 18:22:40 by marvin            #+#    #+#             */
-/*   Updated: 2020/12/07 10:22:52 by marvin           ###   ########.fr       */
+/*   Updated: 2020/12/07 18:28:46 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	redirect(char *str)
+/* static int	redirect(char *str)
 {
 	int		append;
 	int		len;
@@ -38,9 +38,9 @@ static int	redirect(char *str)
 	else
 		fd = open(str, O_RDWR | O_CREAT | O_TRUNC, 0666);
 	return (fd);
-}
+} */
 
-static int	set_fd(char *str)
+/* static int	set_fd(char *str)
 {
 	int fd;
 
@@ -52,18 +52,49 @@ static int	set_fd(char *str)
 	if (fd < 0)
 		write(1, "Couldn't open file\n", 19);
 	return (fd);
+} */
+
+static int	set_fd(t_data *param)
+{
+	int		i;
+	int		fd;
+	char	c;
+
+	i = 0;
+	while (param->argv[i] && ft_memcmp(param->argv[i], ">", 2)
+			&& ft_memcmp(param->argv[i], ">>", 3))
+		i++;
+	if (!param->argv[i])
+		return (1);
+	else if (!ft_memcmp(param->argv[i], ">", 2))
+		return (open(param->argv[i + 1], O_RDWR | O_CREAT | O_TRUNC, 0666));
+	fd = open(param->argv[i + 1], O_RDWR | O_CREAT | O_APPEND, 0666);
+	while ((i = read(fd, &c, 1)))
+		if (i == -1)
+		{
+			write(1, "Couldn't read file\n", 19);
+			break ;
+		}
+	return (fd);
 }
 
 char		**check_command(char *str, t_data *param)
 {
 	int		fd;
+	char	**aux;
 
 	param->argc = count_args(str);
 	param->argv = (char **)ft_calloc(sizeof(char *), (param->argc + 1));
 	set_args(param->argv, str, param->argc);
 	if (param->argv[0] && *(param->argv[0]))
 	{
-		fd = set_fd(str);
+		fd = set_fd(param);
+		aux = copy_args(param);
+		free_matrix(param->argv);
+		param->argv = aux;
+		param->argc = 0;
+		while (param->argv[param->argc])
+			(param->argc)++;
 		param->ret = check_builtins(fd, param);
 		if (param->ret && (param->ret = check_bin(fd, param)))
 		{
